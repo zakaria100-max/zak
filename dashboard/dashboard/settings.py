e1bdd02411272d616652a5f460c76205fbe8b1d9
test <awds1234@outlook.de>
2020-03-13 10:14:52 +0100

    test

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&w)6ayz@msqi-=l)tz3@+oa$qswe9_1fbkl0+br+f$(fa6f%&g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myappzakaria.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,21 +118,5 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+# https://docs.djangoproject.com/en/3.0/howto/ 
 STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
